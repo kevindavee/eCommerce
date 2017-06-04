2abe78a775bbef5df73814a79f83a5c1956093b9
Jerry <Jerry@JERRY-PC>
2017-06-04 23:14:36 +0700

    home

--- eCommerce.Web/Controllers/HomeController.cs
+++ eCommerce.Web/Controllers/HomeController.cs
@@ -5,21 +5,40 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using eCommerce.DAL.Repositories.Brands;
 using Microsoft.AspNetCore.Authorization;
+using eCommerce.DAL.Repositories.The_Products.Products;
+using eCommerce.Web.Models.HomeViewModels;
 
 namespace eCommerce.Web.Controllers
 {
     public class HomeController : Controller
     {
         private BrandRepo brandRepo;
+        private ProductRepo productRepo;
+
         string userName = "";
 
-        public HomeController(BrandRepo _brandRepo)
+        public HomeController(BrandRepo _brandRepo, ProductRepo _productRepo)
         {
             brandRepo = _brandRepo;
+            this.productRepo = _productRepo;
         }
-        public IActionResult Index()
+        public IActionResult Index(long brandId = 0, decimal minPrice = 0, decimal maxPrice = 0)
         {
-            var brandList = brandRepo.GetAll();
+            var brandList = brandRepo.GetAll().ToList();
+
+            //get product list 
+            //bisa difilter lewat brand juga
+            //untuk filter price-nya masih bingung price taro dimana.
+            var productList = productRepo.GetAll().Where(j => brandId == 0 ? true : j.BrandId == brandId).ToList();
+
+            //untuk product harus pake paging. 
+            //bisa paging dengan sakura.pagedlist
+            //bisa juga paging dengan ReflectionIT.Mvc.Paging. 
+            var model = new HomeViewModel();
+            model.BrandList = brandList;
+            model.ProductList = productList;
+            
+
             return View();
         }
     }
---
+++ eCommerce.Web/Models/HomeViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using eCommerce.Core.CommerceClasses.Brands;
+using eCommerce.Core.CommerceClasses.The_Products.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Web.Models.HomeViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Product> ProductList { get; set; }
+        public List<Brand> BrandList { get; set; }
+    }
+}
