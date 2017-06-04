e32f85855287c00d4cf6ef3536066f967c3e9f78
Kevin Dave <kevindaveh@hotmail.com>
2017-06-04 15:13:02 +0700

    New Migration

--- eCommerce.Commons/EntityBase.cs
+++ eCommerce.Commons/EntityBase.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace eCommerce.Commons
 {
     public class EntityBase
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; } = 0;
         public string CreatedBy { get; set; } = "";
         public DateTime CreatedDate { get; set; } = DateTime.Today;
