87248a5824a0e4be592bc532ce2a997c3acb55e3
Kevin Dave <kevindaveh@hotmail.com>
2017-06-09 11:53:24 +0700

    UI checkout, UI payment confirmation

--- eCommerce.Web/wwwroot/js/Custom/transaction-jquery.js
+++ eCommerce.Web/wwwroot/js/Custom/transaction-jquery.js
@@ -35,4 +35,70 @@
             e.preventDefault();
         }
     });
+
+    $('#dropdown-alamat').on('change', function () {
+        var alamatId = parseInt($(this).val());
+        var url = "/Transaction/GetAlamatDetail";
+
+        if (alamatId == 0) {
+            $('#ShippingDetail_AlamatPengiriman').val('');
+            $('#ShippingDetail_Kota').val('');
+            $('#ShippingDetail_Provinsi').val('');
+            $('#ShippingDetail_KodePos').val('');
+        }
+        else {
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: { AlamatId: alamatId },
+                dataType: 'json',
+                success: function (data) {
+                    $('#ShippingDetail_AlamatPengiriman').val(data.theAlamat);
+                    $('#ShippingDetail_Kota').val(data.kota);
+                    $('#ShippingDetail_Provinsi').val(data.provinsi);
+                    $('#ShippingDetail_KodePos').val(data.kodePos);
+                }
+            });
+        }
+    })
+
+    //Page Checkout
+    $('#ShippingDetail_NamaPenerima').on('keyup', function () {
+        EnableSubmitButton('#btn-submit-order');
+    });
+
+    $('.required-dropdown').on('change', function () {
+        EnableSubmitButton('#btn-submit-order');
+    });
+
+    //Page Payment Confirmation
+    $('#BankId').on('change', function () {
+        EnableSubmitButton('#btn-submit-confirmation');
+    });
+
+    $('.required-field').on('keyup', function () {
+        EnableSubmitButton('#btn-submit-confirmation');
+    });
+
+    function EnableSubmitButton(buttonName) {
+        var disabled = CheckRequiredField();
+
+        if (!disabled) {
+            $(buttonName).removeAttr('disabled');
+        }
+        else {
+            $(buttonName).attr('disabled', 'disabled');
+        }
+    }
+
+    function CheckRequiredField() {
+        var required = false;
+        $('.required-field').each(function () {
+            var value = $(this).val();
+            if (value === '0' || value === '') {
+                required = true;
+            }
+        });
+        return required;
+    }
 });
